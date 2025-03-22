dfd77ff71d48b98a21647a54d640e593fbad1f5b
Yohan744 <73723399+Yohan744@users.noreply.github.com>
2025-03-22 22:52:16 +0100

    new nuxt config

--- apps/front/nuxt.config.ts
+++ apps/front/nuxt.config.ts
@@ -3,17 +3,45 @@ import dotenv from 'dotenv'
 dotenv.config({ path: '../../.env' })
 
 export default defineNuxtConfig({
+  compatibilityDate: '2025-03-22',
   ssr: false,
-  compatibilityDate: '2024-11-01',
   rootDir: './',
   css: ['./assets/scss/main.scss'],
   devtools: { enabled: true },
+
+  nitro: {
+    preset: 'vercel',
+    minify: true,
+    compressPublicAssets: {
+      brotli: true
+    }
+  },
+
+  vite: {
+    json: {
+      stringify: true
+    },
+    build: {
+      cssMinify: true,
+      ssrManifest: true,
+      minify: "terser"
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          // additionalData: `@import "@/assets/scss/variables.scss";` TODO CHANGE LATER
+        }
+      }
+    }
+  },
+
   modules: [
     '@pinia/nuxt',
     'pinia-plugin-persistedstate/nuxt',
   ],
+
   piniaPluginPersistedstate: {
     storage: 'localStorage',
     debug: false
-  }
+  },
 })
