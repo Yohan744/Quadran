8cd2add69d58f1f4a9f13a59c20d3a07d655c9ad
Yohan744 <73723399+Yohan744@users.noreply.github.com>
2025-03-28 23:55:20 +0100

    deleted possibility to pinch zoom

--- apps/front/app.vue
+++ apps/front/app.vue
@@ -3,21 +3,37 @@
 </template>
 
 <script setup lang="ts">
-  import { useSocketStore } from '~/stores/socketStore'
-  import { onMounted, onBeforeUnmount } from 'vue'
+  import { useSocketStore } from '~/stores/socketStore';
+  import { onMounted, onBeforeUnmount } from 'vue';
 
-  const socketStore = useSocketStore()
+  const socketStore = useSocketStore();
 
   const handleBeforeUnload = () => {
-    socketStore.clearSocket()
-  }
+    socketStore.clearSocket();
+  };
 
   onMounted(() => {
-    window.addEventListener('beforeunload', handleBeforeUnload)
-  })
+    window.addEventListener('beforeunload', handleBeforeUnload);
+    document.addEventListener('gesturestart', disableZoom);
+    document.addEventListener('gesturechange', disableZoom);
+    document.addEventListener('gestureend', restoreZoom);
+  });
 
   onBeforeUnmount(() => {
-    window.removeEventListener('beforeunload', handleBeforeUnload)
-    socketStore.clearSocket()
-  })
+    window.removeEventListener('beforeunload', handleBeforeUnload);
+    document.removeEventListener('gesturestart', disableZoom);
+    document.removeEventListener('gesturechange', disableZoom);
+    document.removeEventListener('gestureend', restoreZoom);
+    socketStore.clearSocket();
+  });
+
+  const disableZoom = e => {
+    e.preventDefault();
+    document.body.style.zoom = '0.999999999';
+  };
+
+  const restoreZoom = e => {
+    e.preventDefault();
+    document.body.style.zoom = '1';
+  };
 </script>
